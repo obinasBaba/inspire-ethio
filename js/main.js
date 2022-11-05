b0f099af1b0b40c14e8f64db3365eebf63d84f90
obinasBaba <henokgetachew500@gmail.com>
2022-11-05 17:18:15 +0300

    responsive fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 import gsap from 'gsap'
 
+let lastScrollTop = 0;
+const body = document.body;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+  let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+
+  if (st > lastScrollTop){
+    console.log('down')
+  } else {
+    console.log('up')
+  }
+
+  lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+
+});
+
 
 const infoBtn = document.querySelector('.slider .info-btn')
 const contentBtn = document.querySelector('.slider .content-btn')
@@ -9,6 +26,8 @@ const content = document.querySelector('.slider .content')
 
 
 infoBtn.addEventListener('click', evt => {
+  contentBtn.classList.remove('active')
+  evt.target.classList.add('active')
   gsap.to(indicator, {
     x: 0,
     duration: .3
@@ -21,6 +40,8 @@ infoBtn.addEventListener('click', evt => {
 })
 
 contentBtn.addEventListener('click', evt => {
+  infoBtn.classList.remove('active')
+  evt.target.classList.add('active')
   gsap.to(indicator, {
     x: '100%',
     duration: .3
